e1e899612b168533a61cfb03da866181eeeb4e43
MatanCor <smatanc@gmail.com>
2020-03-20 04:19:40 +0200

    Adding idt

--- kernel/util.h
+++ kernel/util.h
@@ -1,2 +1,6 @@
 void memory_copy(char *source, char *dest, int nbytes);
 void int_to_ascii(int n, char str[]);
+
+
+#define low_16(address) (unsigned short)((address) & 0xFFFF)
+#define high_16(address) (unsigned short)(((address) >> 16) & 0xFFFF)
